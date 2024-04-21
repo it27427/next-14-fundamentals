354d11e1e8294109cf51e5d8fca59c641ddfa900
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-21 11:17:04 +0600

    tainwind-css-container configured

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer>
-      <div className='container'>
+      <div className='container px-4 lg:px-12 xxl:px-0'>
         <p>&copy; {currentYear} All rights reserved - IT</p>
       </div>
     </footer>
