429202490c79feb323db3f9e51511b2095a2050d
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-24 01:45:26 +0600

    blogs page

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,12 +1,12 @@
-const BlogDetails = ({ params }) => {
-  const { id, title, description } = params;
+const BlogDetails = ({ params, title, description }) => {
+  const { id } = params;
 
   return (
     <>
       <div className='flex flex-col gap-3'>
         <h1 className='text-xl font-bold'>This is blog {id}</h1>
-        {/* <h1 className='text-xl font-bold'>{title}</h1>
-        <p>{description}</p> */}
+        <h1 className='text-xl font-bold'>{title}</h1>
+        <p>{description}</p>
       </div>
     </>
   );
