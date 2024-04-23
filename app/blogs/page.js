adfff5e66499e897d2f7fa1b59190c1887b2d5e8
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-24 00:41:48 +0600

    about and blogs page added

---
+++ app/blogs/page.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const blogs = [
+  {
+    id: 1,
+    title: 'This is blog one',
+    description: 'Blog description one',
+  },
+  {
+    id: 2,
+    title: 'This is blog two',
+    description: 'Blog description two',
+  },
+  {
+    id: 3,
+    title: 'This is blog three',
+    description: 'Blog description three',
+  },
+  {
+    id: 4,
+    title: 'This is blog four',
+    description: 'Blog description four',
+  },
+  {
+    id: 5,
+    title: 'This is blog five',
+    description: 'Blog description five',
+  },
+];
+
+const BlogsPage = () => {
+  return (
+    <>
+      <h1>Blogs Page</h1>
+    </>
+  );
+};
+
+export default BlogsPage;
