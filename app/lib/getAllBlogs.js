d9b1d20d9b839c4939e32468a97a5cf9ec90eff2
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-24 12:35:24 +0600

    fetchAllBlogs

--- app/lib/getAllBlogs.js
+++ app/lib/getAllBlogs.js
@@ -1,3 +1,11 @@
-const getAllBlogs = () => {};
+const getAllBlogs = async () => {
+  const response = await fetch(
+    'https://newsapi.org/v2/everything?q=apple&from=2024-04-23&to=2024-04-23&sortBy=popularity&apiKey=237deac863ac48918c9193e99bdad82d'
+  );
+
+  const data = response.json();
+
+  return data;
+};
 
 export default getAllBlogs;
