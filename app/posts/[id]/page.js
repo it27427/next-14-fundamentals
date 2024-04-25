a94efec2d8b67b82f4dfd6039165c913caf3ab32
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 09:40:11 +0600

    getSinglePost dynamically added

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,9 +1,13 @@
-const PostDetails = ({ params }) => {
+import getPost from '@/app/lib/getPost';
+
+const PostDetails = async ({ params }) => {
   const { id } = params;
+  const post = await getPost(id);
 
   return (
-    <div>
-      <h1 className='font-bold text-3xl'>Post number is {id}</h1>
+    <div className='flex flex-col gap-4'>
+      <h1 className='font-bold text-3xl'>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
