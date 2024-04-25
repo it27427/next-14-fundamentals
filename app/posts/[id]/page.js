14fe61ece5e014de5c154070eef9206cab726abf
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-25 10:48:05 +0600

    fetch-post-comments

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,4 +1,5 @@
 import getPost from '@/app/lib/getPost';
+import getPostComments from '@/app/lib/getPostComments';
 
 export async function generateMetadata({ params }) {
   const { id } = params;
@@ -12,12 +13,34 @@ export async function generateMetadata({ params }) {
 
 const PostDetails = async ({ params }) => {
   const { id } = params;
-  const post = await getPost(id);
+  const postPromise = getPost(id);
+  const commentsPromise = getPostComments(id);
+
+  const [post, comments] = await Promise.all([postPromise, commentsPromise]);
 
   return (
-    <div className='flex flex-col gap-4'>
-      <h1 className='font-bold text-3xl text-indigo-500'>{post.title}</h1>
-      <p>{post.body}</p>
+    <div className='flex flex-col gap-6'>
+      <div className='flex flex-col gap-4'>
+        <h1 className='font-bold text-3xl text-indigo-500'>{post.title}</h1>
+        <p>{post.body}</p>
+      </div>
+
+      <hr className='border-violet-500' />
+
+      <div className='flex flex-col gap-4'>
+        <h2 className='font-bold text-2xl text-neutral-700'>Comments</h2>
+
+        <ul className='flex flex-col gap-3'>
+          {comments.map((comment) => (
+            <li key={comment.id} className='flex flex-col gap-2'>
+              <span className='inline-block font-semibold text-violet-500'>
+                {comment.name}
+              </span>
+              <span>{comment.body}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
